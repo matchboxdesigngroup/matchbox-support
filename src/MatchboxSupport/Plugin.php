d8db77b6db1342e495c88744b64fe81268574aad
Eugene Kyale <eugene.kyale@gmail.com>
2025-06-02 16:13:35 +0300

    Add disable_block_recommendations method to prevent block recommendations in the editor

--- src/MatchboxSupport/Plugin.php
+++ src/MatchboxSupport/Plugin.php
@@ -85,6 +85,7 @@ class Plugin {
 
 		add_action( 'admin_menu', [ $this, 'settings_menu' ] );
 		add_action( 'admin_init', [ $this, 'settings_init' ] );
+		add_action( 'admin_init', [ $this, 'disable_block_recommendations' ] );
 
 		$this->api = new API();
 		$this->api->init();
@@ -109,7 +110,7 @@ class Plugin {
 		wp_localize_script(
 			'matchbox-helpscout-beacon',
 			'matchbox_helpscout_params',
-			[ 
+			[
 				'beacon_id' => get_option( 'matchbox_helpscout_beacon_id', '' ),
 			]
 		);
@@ -134,7 +135,7 @@ class Plugin {
 		wp_localize_script(
 			'matchbox-helpscout-beacon',
 			'matchbox_helpscout_params',
-			[ 
+			[
 				'beacon_id' => get_option( 'matchbox_helpscout_beacon_id', '' ),
 			]
 		);
@@ -153,13 +154,13 @@ class Plugin {
 		$svg_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3l58.3 0c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24l0-13.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1l-58.3 0c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>';
 
 		$wp_admin_bar->add_node(
-			[ 
-				'id' => 'matchbox_helpscout_toggle',
-				'title' => '<div class="matchbox-support-pill-toggle" id="matchbox-support-pill-toggle"><div class="toggle-icon">' . $svg_icon . '</div></div>',
-				'href' => '#',
-				'meta' => [ 
+			[
+				'id'     => 'matchbox_helpscout_toggle',
+				'title'  => '<div class="matchbox-support-pill-toggle" id="matchbox-support-pill-toggle"><div class="toggle-icon">' . $svg_icon . '</div></div>',
+				'href'   => '#',
+				'meta'   => [
 					'onclick' => 'matchboxToggleHelpscout(); return false;',
-					'title' => esc_attr__( 'Show or hide the Matchbox Support overlay', 'matchbox' ),
+					'title'   => esc_attr__( 'Show or hide the Matchbox Support overlay', 'matchbox' ),
 				],
 				'parent' => 'top-secondary',
 			]
@@ -356,5 +357,16 @@ class Plugin {
 	public function helpscout_beacon_id_field_cb() {
 		$value = get_option( 'matchbox_helpscout_beacon_id' );
 		echo '<input type="text" id="matchbox_helpscout_beacon_id" name="matchbox_helpscout_beacon_id" value="' . esc_attr( $value ) . '" />';
+	}
+
+	/**
+	 * Disables block recommendations from 3rd-party plugins in the block editor.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function disable_block_recommendations() {
+		remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
 	}
 }
